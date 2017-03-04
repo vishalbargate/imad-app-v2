093a1888beefd269bf8c6310959e284dfee3a313
vishalbargate <vishalbargate@gmail.com>
2017-03-04 14:11:44 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -8,6 +8,12 @@ element.innerHTML = 'new value';
  
  //move the image
  var img = document.getElementById('madi');
+ var marginLeft =0;
+ function moveRight() {
+     margingLeft = marginLeft + 10;
+      img.style.marginLeft = margingLeft + 'px';
+ }
   img.onclick = function() {
-   img.style.marginLeft = '100px';   
+      var interval = setInterval(moveRight, 100);
+
   };
