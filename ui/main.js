46999963c0dd24397eb7ff7acf578e162f6fcc76
vishalbargate <vishalbargate@gmail.com>
2017-03-04 13:55:20 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -5,3 +5,9 @@ var element = document.getElementById('main-text'
 );
 
 element.innerHTML = 'new value';
+ 
+ //move the image
+ var img = document.getElementById('img');
+  img.onclick = function() {
+   img.style.marginLeft = '100px';   
+  };
